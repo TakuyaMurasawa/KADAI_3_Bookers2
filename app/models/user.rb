c4821640ae99e790570b732761d95d066d305c11
TakuyaMurasawa <bandage104.batt1e37@gmail.com>
2021-06-21 14:30:57 +0000

    [ADD]ログイン画面

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+        # :authentication_keys => [:username]
+
+  validates :name, uniqueness: true
+  validates :name, presence: true
+
+  # def self.find_first_by_auth_conditions(warden_conditions)
+  #   conditions = warden_conditions.dup
+  #   if login = conditions.delete(:login)
+  #     #認証の条件式を変更する
+  #     where(conditions).where(["name = :value", { :value => name }]).first
+  #   else
+  #     where(conditions).first
+  #   end
+  # end
+
+  has_many :books, dependent: :destroy
+
+  attachment :profile_image
 end
