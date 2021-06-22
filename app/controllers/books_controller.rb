453e142118e2ccb5941617487a42d6a82348d4da
TakuyaMurasawa <bandage104.batt1e37@gmail.com>
2021-06-22 14:46:22 +0000

    [UPDATE]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,26 +1,30 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
-  
+
   def index
+    @book = Book.new
     @books = Book.all
   end
 
   def create
-    @book = Book.find(params[:id])
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
     @book.save
+    
     redirect_to book_path(@book.id)
   end
 
   def show
+    @new_book = Book.new
     @book = Book.find(params[:id])
+    # Nに値するbookのuser_idカラムをもとにUserからuse.idを探す
+    # @user = User.find(params[:user_id])
+    # @user = @book.user
   end
-  
+
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     @book = Book.find(params[:id])
     @book.update(book_params)
@@ -30,11 +34,11 @@ class BooksController < ApplicationController
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
-    redirect_to 
+    redirect_to
   end
-  
+
   private
-  
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
   def show
+    @book = Book.new
     @user = User.find(params[:id])
-    @books = @user.books.all
+  end
+
+  def index
+    @book = Book.new
+    # @user = current_user
+    @users = User.all
   end
 
   def edit
